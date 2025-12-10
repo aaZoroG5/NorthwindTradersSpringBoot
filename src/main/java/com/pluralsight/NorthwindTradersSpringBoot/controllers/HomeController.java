e7aafcd90872164a9f89d1cf02d5178bc606e701
andyariza <andyariza90@gmail.com>
2025-12-10 15:33:34 -0500

    added post mapping annot in home controller

--- src/main/java/com/pluralsight/NorthwindTradersSpringBoot/controllers/HomeController.java
+++ src/main/java/com/pluralsight/NorthwindTradersSpringBoot/controllers/HomeController.java
@@ -5,6 +5,8 @@ import com.pluralsight.NorthwindTradersSpringBoot.models.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -26,4 +28,8 @@ public class HomeController {
         return productDao.getAll();
     }
 
+    @PostMapping("/products")
+    public void addProduct(@RequestBody Product product){
+        productDao.add(product);
+    }
 }
